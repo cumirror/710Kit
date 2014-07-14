3a3baa8b43f0b5aad7509f6b8968cc785c209f36
710leo <710leo@users.noreply.github.com>
2014-07-14 08:18:47 -0700

    update

--- plugins/portscan.py
+++ plugins/portscan.py
@@ -3,15 +3,18 @@
 # code by 710
 from socket import gethostbyname,socket,AF_INET,SOCK_STREAM
 from urlparse import urlparse
-from threading import Thread
+from threading import Thread,Lock
 import Queue
 import time
 
+threadLock = Lock()
+
 class portScan(Thread):
-	def __init__(self, host, port):
+	def __init__(self, host, port,result_file):
 		Thread.__init__(self)
 		self.host_list = host
 		self.port = port
+		self.result = result_file
 
 	def run(self):
 		while True:
@@ -28,15 +31,25 @@ class portScan(Thread):
 				s.settimeout(3)
 				s.connect((ip,self.port))
 				print "%s of %s is open" % (self.port,ip)
+				threadLock.acquire()
+				self.result.write(ip+"\n")
+				threadLock.release()
+				
 			except:
 				print "%s error" % ip
 			
 def port_scan(port):
+	result_file = openfile('mult_domains_port_result')
+	result_file.write("the websites of %s is open:\n" % port)
 	jobs = Queue.Queue(0)
 	for url in open('../dict/urls'):
 		jobs.put(url.rstrip()) #將文本加入到队列中
 	for x in range(10): #创建10个线程
-		t = portScan(jobs,port).start()
-		time.sleep(1)
+		t = portScan(jobs,port,result_file).start()
+		#time.sleep(1)
 	return True
+def openfile(filename):
+	f = open(filename,'w')
+	return f
+
 port_scan(80)
