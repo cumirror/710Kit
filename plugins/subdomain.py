30973460df181a9889b9b82fe8c0944e93a16256
tongjin <tongjinam@qq.com>
2014-10-21 11:49:23 +0800

    Update subdomain.py
    add methods:
    	get_sub_of_chinaz  ---- not available now
    	try_sub_by_dns     ---- you need a dict

--- plugins/subdomain.py
+++ plugins/subdomain.py
@@ -2,42 +2,55 @@
 #encoding: utf-8
 #code by 710
 
+#add methods:
+#	get_sub_of_chinaz  ---- not available now
+#	try_sub_by_dns     ---- you need a dict
+#by cumirror 2014/10/21
+
 import re
 import requests
-from BeautifulSoup import BeautifulSoup
+import bs4
+from bs4 import BeautifulSoup
+import socket
 
-def get_subdomain(url):	
-	'''
-	查询网站子域名
-	'''
-	results = open('dict/domain','w+');
+def get_ip_from_subdomain(url):
+        try:
+            ip = socket.gethostbyname(url)
+            return ip
+        except socket.gaierror:
+            return ""
 
-	link1 = get_sub_of_links(url)
-	link2 = get_sub_of_baidu(url,5)
-	link3 = get_sub_of_bing(url,10)
-	links = link1 + link2 + link3
-	links = list(set(links))
-	for link in links:
-		print link
-		link = link + '\n'
-		results.write(link)
-	results.close()
+def get_result(url):
+        return url + ',' + get_ip_from_subdomain(url)
 
 def get_sub_of_links(url):
-	print "links"
+	print("links-------")
 	result = []
-
 	source = requests.get('http://i.links.cn/subdomain/'+ url +'.html')
-	#print source.text
+	#print(source.text)
 	links = re.findall(r'(value="http://)([^>]+?)(">)',source.text)
 	for link in links:
 		domain = link[1].split('/')
-		result.append(domain[0])
-		print domain[0]
+		result.append(get_result(domain[0]))
+		#print(domain[0])
+	return result
+
+#not available now
+def get_sub_of_chinaz(url):
+	print("chinaz-------")
+	result = []
+	payload = 's=' + url
+	source = requests.post("http://s.tool.chinaz.com/same",data=payload)
+	print(source.text)
+	links = re.findall(r'(a herf=\'http://)([^\']+?)(\')',source.text)
+	for link in links:
+		domain = link[1].split('/')
+		result.append(get_result(domain[0]))
+		#print(domain[0])
 	return result
 	
 def get_sub_of_baidu(url,page):
-	print "Baidu-------"
+	print("Baidu-------")
 	result = []
 	i = 1
 	while i <= page:
@@ -48,13 +61,13 @@ def get_sub_of_baidu(url,page):
 		links = re.findall(r'(<span class="g">)([^>]+?)(/&nbsp;)',source.text)
 		for link in links:
 			domain = link[1].split('/')
-			result.append(domain[0])
-			print domain[0]
+			result.append(get_result(domain[0]))
+			#print(domain[0])
 		i = i+1
 	return result
 	
 def get_sub_of_bing(url,page):
-	print "bing-----"
+	print("bing-----")
 	result = []
 	i = 1
 	while i < page:
@@ -66,7 +79,34 @@ def get_sub_of_bing(url,page):
 		for link in links:
 			temp = link[1].strip('http://')
 			domain = temp.split('/')
-			result.append(domain[0])
-			print domain[0]
+			result.append(get_result(domain[0]))
+			#print(domain[0])
 		i = i+1
 	return result
+
+def try_sub_by_dns(url):
+    print("DNS-----")
+    sub_domain = open('dict.txt', 'rb')
+    result = []
+    for line in sub_domain:
+        new_url = line.decode('utf-8').strip('\r\n') + '.' + url
+        result.append(get_result(new_url))
+        #print(new_url)
+    return result
+
+def get_subdomain(url):	
+	'''
+	查询网站子域名
+	'''
+	results = open('result.txt','w+')
+	link1 = get_sub_of_links(url)
+	link2 = get_sub_of_baidu(url,5)
+	link3 = get_sub_of_bing(url,10)
+	link4 = try_sub_by_dns(url)
+	links = link1 + link2 + link3 + link4
+	links = list(set(links))
+	for link in links:
+		#print(link)
+		link = link + '\n'
+		results.write(link)
+	results.close()
